63764d52b90087181fb5519ba315fa2ef8528947
MFbuider <62496746+MFbuider@users.noreply.github.com>
2020-11-28 16:39:27 +0800

    重整[PC(exe)]

---
+++ moon_2D_game-/2D_game/Assets/script/c.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class c : MonoBehaviour
+{
+    [Header("cc"), Tooltip("用來設定cc的。")]
+    [Range(0, 3000)]
+    public int cc = 3000;
+public float g = 1000f;[Range(0, 5000)]
+    public  string a = "bc";
+    public bool haveatree = true;
+    public Color red = Color.red;
+    public Color clorw = new Color(0, 2, 0);
+   public Vector2 v2 = Vector2.one;
+   public Vector3 v3 = Vector3.one;
+   public Vector4 v4 = Vector4.one;
+   public GameObject obj;
+   public Terrain tr;
+   public SpriteRenderer sr ;
+
+    private void Start()
+    {
+        print("HiPlear");
+    }
+
+    private void Update()
+    {
+        print("Update");
+    }
+    
+}
---
+++ moon_2D_game-/2D_game/Assets/script/plear.cs
@@ -0,0 +1,55 @@
+﻿
+using UnityEngine;
+
+/// <summary>
+/// 移動
+/// </summary>
+public class plear : MonoBehaviour
+{
+    [Header("速度"), Tooltip("用來設定移動的速度。")]
+    public float speed = 10.5f;[Range(0, 1000)]
+    [Header("跳躍"), Tooltip("用來設定跳躍的高度。")]
+    public float jump = 100f;[Range(0, 3000)]
+    public bool floor = false;
+    int score = 0;     
+    public GameObject bullet;
+    public GameObject 子彈生成點;
+    public float Movingpeed = 800f;[Range(0, 5000)]
+    public AudioClip bulletAU;
+    public int lives = 3;[Range(0, 5)]
+    private AudioSource aud;
+    private Rigidbody2D rig;
+    private Animator ani;
+
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody2D>(); 
+    }
+    private void Update()
+    {
+        Move();   
+    }
+    private void Move()
+    {
+        float L = Input.GetAxis("Horizontal");
+        rig.velocity = new Vector2(L * speed, rig.velocity.y);
+    }
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    public void jump2()
+    {
+   
+    }
+    /// <summary>
+    /// 開槍
+    /// </summary>
+    public void fire()
+    {
+
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
